436687b5baafcfe304f54206c8c1076fd0aecb90
LE2A_04_Yuki_iida <k021g1333@g.neec.ac.jp>
2022-11-02 09:59:12 +0900

    あ

--- main.cpp
+++ main.cpp
@@ -20,31 +20,19 @@ int main()
 {
 	printf("\n「丁半博打」0キー:偶数 1キー:奇数\n");
 
-	//ラムダ式
-	std::function<void(int)> ShowResult = [](int number)
-	{
-		if (number == num(engine)) printf("当たり！\n");
-		else                       printf("はずれ…\n");
-	};
-
-	std::function<void(std::function<void(int)>, int, int)> SetTimeOut
-		= [](std::function<void(int)> fx, int number, int second)
-	{
-		second *= 1000;
-
-		Sleep(second);
-
-		fx(number);
-	};
-
-
 	//入力
 	int number = 0;
 	scanf_s("%d", &number);
 
+	//ラムダ式
+	std::function<void()> ShowResult = [=]() { number == num(engine) ? printf("当たり！\n") : printf("はずれ…\n"); };
+
+	std::function<void(std::function<void()>, int)> SetTimeOut
+		= [](std::function<void()> fx, int second) { second *= 1000; Sleep(second); fx(); };
+
 	const int waitTime = 3;
 	//関数
-	SetTimeOut(ShowResult, number, waitTime);
+	SetTimeOut(ShowResult, waitTime);
 
 
 	return 0;
