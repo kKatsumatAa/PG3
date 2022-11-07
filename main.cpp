c2b666bb36fa003fd0dc4fd21186acad9bcff9c5
LE2A_04_Yuki_iida <k021g1333@g.neec.ac.jp>
2022-11-07 11:45:16 +0900

    削除、編集する関数追加

--- main.cpp
+++ main.cpp
@@ -12,123 +12,127 @@ typedef struct cell
 
 void Create(CELL* currentCell, int insertNum)
 {
-	CELL* endCell = currentCell;
-
 	CELL* newCell;
 	//新規作成するセル分のメモリを確保
 	newCell = (CELL*)malloc(sizeof(CELL));
 
 	newCell->val = insertNum;
 	//配置する場所にもともとあったセルのアドレスを入れる
-	newCell->prev = endCell;
+	newCell->prev = currentCell;
 	//配置する場所にもともとあったセルの、次にあるセルのアドレスを入れる
-	newCell->next = endCell->next;
+	newCell->next = currentCell->next;
 
 	//挿入
-	if (endCell->next)
+	if (currentCell->next)
 	{
-		CELL* nextCell = endCell->next;
+		CELL* nextCell = currentCell->next;
 		//配置する場所にもともとあったセルの、
 		//次にあるセルのprevに新しく作るセルのアドレス
 		nextCell->prev = newCell;
 	}
 	//配置する場所にもともとあったセルのnextに新しく作るセルのアドレス
-	endCell->next = newCell;
+	currentCell->next = newCell;
 }
 
-void Index(CELL* cellEnd)
+void Index(CELL* cellStart)
 {
 	int no = 1;
 	//最後のcellになるまで出力
-	while (cellEnd->next != nullptr)
+	while (cellStart->next != nullptr)
 	{
-		cellEnd = cellEnd->next;
-		printf("%d ", no);
-		printf("%p ", cellEnd->prev );
-		printf("%5d ", cellEnd->val );
-		printf("(%p) ", cellEnd );
-		printf("%p \n", cellEnd->next );
+		cellStart = cellStart->next;
+		printf("no:%d ", no);
+		printf("prev:%p ", cellStart->prev);
+		printf("val:%5d ", cellStart->val);//五桁右揃え
+		printf("this:(%p) ", cellStart);
+		printf("next:%p \n", cellStart->next);
 		no++;
 	}
 }
 
-CELL* GetInsertCellAddres(CELL* cellEnd, int cellNum)
+CELL* GetInsertCellAddres(CELL* cellStart, int cellNum)
 {
-	CELL* endCell = cellEnd;
-
-	//cellNumの回数分進む
-	for (int i = 0; i < cellNum; i++)
+	//n番目に入れたいときはn番目にもともとあったセルの前に入れてあげる（-1）
+	for (int i = 0; i < cellNum - 1; i++)
 	{
-		if (endCell->next)
+		if (cellStart->next)
 		{
 			//最後のセルじゃないとき
-			endCell = endCell->next;
+			cellStart = cellStart->next;
 		}
 		else
 		{
 			break;
 		}
 	}
-	return endCell;
+	return cellStart;
 }
 
-//----------------------------------------
-typedef struct cellstr
+void DeleteCell(CELL* cellStart, int cellNum)
 {
-	const char* str;
-	struct cellstr* next = nullptr;//自己参照構造体
-
-}CELLSTR;
-
-void Create(CELLSTR* cellStart, const char* insertStr)
-{
-	CELLSTR* newCell;
-	//新規作成するセル分のメモリを確保
-	newCell = (CELLSTR*)malloc(sizeof(CELLSTR));
-
-	newCell->str = insertStr;
-	newCell->next = nullptr;
-
-	//最後のセルのアドレスの一つ目の処理は引数から持ってきた
-	//リストのうち最初のセルのアドレスが該当する
-	while (cellStart->next != nullptr)
+	//cellNumの回数分進む
+	for (int i = 0; i < cellNum; i++)
 	{
-		cellStart = cellStart->next;
+		if (cellStart->next)
+		{
+			//最後のセルじゃないとき
+			cellStart = cellStart->next;
+		}
+		else
+		{
+			break;
+		}
 	}
-	cellStart->next = newCell;
+	//削除したいセルの一個前のセルのnextに,
+	//削除したいセルの次のセルのアドレスを入れてあげる
+	cellStart->prev->next = cellStart->next;
+	//削除したいセルの次のセルのprevに,
+	//削除したいセルの一個前のセルのアドレスを入れてあげる
+	cellStart->next->prev = cellStart->prev;
+
+	//削除
+	delete cellStart;
 }
 
-void Index(CELLSTR* cellStart)
+
+void EditCell(CELL* cellStart, int cellNum, int insertNum)
 {
-	//最後のcellになるまで出力
-	while (cellStart->next != nullptr)
+	//cellNumの回数分進む
+	for (int i = 0; i < cellNum; i++)
 	{
-		printf("%s\n", cellStart->str);
-		cellStart = cellStart->next;
+		if (cellStart->next)
+		{
+			//最後のセルじゃないとき
+			cellStart = cellStart->next;
+		}
+		else
+		{
+			break;
+		}
 	}
-	printf("%s\n", cellStart->str);
+
+	//中身を変える
+	cellStart->val = insertNum;
 }
+
 
 int main()
 {
 	CELL cellStart;
-	cellStart.val = 1;
 
 	for (int i = 0; i < 10; i++)
 	{
-		Create(&cellStart, i + 1);
+		Create(GetInsertCellAddres(&cellStart, i + 1), i + 1);
 	}
+
+	Create(GetInsertCellAddres(&cellStart, 5), 114514);
+
+	//DeleteCell(&cellStart, 5);
+
+	//EditCell(&cellStart, 5, 514);
 
 	Index(&cellStart);
 
-	//-------------------------
-	CELLSTR cellStrStart;
-	cellStrStart.str = "パパ";
-	Create(&cellStrStart, "ママ");
-	Create(&cellStrStart, "ポチex");
-	Create(&cellStrStart, "タマ大魔王");
-
-	Index(&cellStrStart);
 
 	return 0;
 }
