db4ecc31b004caab0ed18ac2f39c364de910cf33
LE2A_04_Yuki_iida <k021g1333@g.neec.ac.jp>
2022-11-07 10:27:06 +0900

    途中

--- main.cpp
+++ main.cpp
@@ -5,37 +5,71 @@
 typedef struct cell
 {
 	int val = 0;
+	struct cell* prev = nullptr;
 	struct cell* next = nullptr;//自己参照構造体
 
 }CELL;
 
-void Create(CELL* cellStart, int insertNum)
+void Create(CELL* currentCell, int insertNum)
 {
+	CELL* endCell = currentCell;
+
 	CELL* newCell;
 	//新規作成するセル分のメモリを確保
 	newCell = (CELL*)malloc(sizeof(CELL));
 
 	newCell->val = insertNum;
-	newCell->next = nullptr;
+	//配置する場所にもともとあったセルのアドレスを入れる
+	newCell->prev = endCell;
+	//配置する場所にもともとあったセルの、次にあるセルのアドレスを入れる
+	newCell->next = endCell->next;
 
-	//最後のセルのアドレスの一つ目の処理は引数から持ってきた
-	//リストのうち最初のセルのアドレスが該当する
-	while (cellStart->next != nullptr)
+	//挿入
+	if (endCell->next)
 	{
-		cellStart = cellStart->next;
+		CELL* nextCell = endCell->next;
+		//配置する場所にもともとあったセルの、
+		//次にあるセルのprevに新しく作るセルのアドレス
+		nextCell->prev = newCell;
 	}
-	cellStart->next = newCell;
+	//配置する場所にもともとあったセルのnextに新しく作るセルのアドレス
+	endCell->next = newCell;
 }
 
-void Index(CELL* cellStart)
+void Index(CELL* cellEnd)
 {
+	int no = 1;
 	//最後のcellになるまで出力
-	while (cellStart->next != nullptr)
+	while (cellEnd->next != nullptr)
 	{
-		printf("%d\n", cellStart->val);
-		cellStart = cellStart->next;
+		cellEnd = cellEnd->next;
+		printf("%d ", no);
+		printf("%p ", cellEnd->prev );
+		printf("%5d ", cellEnd->val );
+		printf("(%p) ", cellEnd );
+		printf("%p \n", cellEnd->next );
+		no++;
 	}
-	printf("%d\n", cellStart->val);
+}
+
+CELL* GetInsertCellAddres(CELL* cellEnd, int cellNum)
+{
+	CELL* endCell = cellEnd;
+
+	//cellNumの回数分進む
+	for (int i = 0; i < cellNum; i++)
+	{
+		if (endCell->next)
+		{
+			//最後のセルじゃないとき
+			endCell = endCell->next;
+		}
+		else
+		{
+			break;
+		}
+	}
+	return endCell;
 }
 
 //----------------------------------------
@@ -82,7 +116,7 @@ int main()
 
 	for (int i = 0; i < 10; i++)
 	{
-		Create(&cellStart, i + 2);
+		Create(&cellStart, i + 1);
 	}
 
 	Index(&cellStart);
