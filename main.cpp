9e3c6808283375334b77a147dd8514654f14b359
LE2A_04_Yuki_iida <k021g1333@g.neec.ac.jp>
2022-11-21 12:05:43 +0900

    途中

--- main.cpp
+++ main.cpp
@@ -4,26 +4,6 @@ int main()
 {
 	CellManager* cellManager = new CellManager;
 
-	{
-		//CELL cellStart;
-
-		//for (int i = 0; i < 10; i++)
-		//{
-		//	Create(GetInsertCellAddres(&cellStart, i + 1), i + 1);
-		//}
-
-		//Create(GetInsertCellAddres(&cellStart, 5), 114514);
-
-		////DeleteCell(&cellStart, 5);
-
-		////EditCell(&cellStart, 5, 514);
-
-		//Index(&cellStart);
-
-		//printf("num:%d", GetCellSize(&cellStart));
-	}
-
-
 	while (true)
 	{
 		cellManager->Draw();
